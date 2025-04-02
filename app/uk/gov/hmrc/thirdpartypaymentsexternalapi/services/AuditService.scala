65c1d0819a4f8d7e00b4849939170540f949a33d
michaelgroves90 <67477479+michaelgroves90@users.noreply.github.com>
2025-04-02 10:06:39 +0100

    Service

--- app/uk/gov/hmrc/thirdpartypaymentsexternalapi/services/AuditService.scala
+++ app/uk/gov/hmrc/thirdpartypaymentsexternalapi/services/AuditService.scala
@@ -1,5 +1,27 @@
 package uk.gov.hmrc.thirdpartypaymentsexternalapi.services
 
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.thirdpartypaymentsexternalapi.models.{ClientJourneyId, TaxRegime}
+import uk.gov.hmrc.thirdpartypaymentsexternalapi.models.audit.{InitiateJourneyAuditDetail, Outcome}
+import java.util.UUID
+
 class AuditService {
+
+  private def toInitiateJourneyResult(
+                                       outcome: Outcome,
+                                       taxRegime: TaxRegime,
+                                       paymentReference: String,
+                                       amount: Int,
+                                       originOfRequest: String,
+                                       clientJourneyId: ClientJourneyId
+                                     )(using HeaderCarrier): Unit =
+    InitiateJourneyAuditDetail(
+      outcome = Outcome(true),
+      taxRegime = String,
+      paymentReference = String,
+      amount = 1,
+      originOfRequest = String,
+      clientJourneyId = ClientJourneyId(UUID)
+    )
 
 }
